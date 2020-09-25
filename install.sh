1b862ec98c2c3195673b89afb29366890b13c0ef
Vincent Lieu <v.lieu93@gmail.com>
2020-09-26 00:37:27 +1000

    added git to package list

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ echo "Installing dotfiles..."
 which -s brew
 if [ $? != 0 ]; then
     echo "Homebrew not found. Installing Homebrew..."
-    # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else
     echo "Homebrew already installed. Updating..."
-    # brew update
-    # brew upgrade
+    brew update
+    brew upgrade
 fi
 
-# echo "Turning off analytics..."
-# brew analytics off
-# export HOMEBREW_NO_ANALYTICS=1
+echo "Turning off analytics..."
+brew analytics off
+export HOMEBREW_NO_ANALYTICS=1
 
 echo "Installing Homebrew packages..."
 
