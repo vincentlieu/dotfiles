fcb7e5ada715fd05243681c664a0d4cd210648bc
Vincent Lieu <v.lieu93@gmail.com>
2020-09-26 00:28:20 +1000

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# vincentlieu dotfiles
+
+# Usage
+
+```
+$ git clone https://github.com/vincentlieu/dotfiles.git ~dotfiles
+$ cd && ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,32 @@
+echo "Installing dotfiles..."
+
+## Install Homebrew and packages
+# $? is used to find the return value of the last executed command. 0 is success.
+
+which -s brew
+if [ $? != 0 ]; then
+    echo "Homebrew not found. Installing Homebrew..."
+    # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    echo "Homebrew already installed. Updating..."
+    # brew update
+    # brew upgrade
+fi
+
+# echo "Turning off analytics..."
+# brew analytics off
+# export HOMEBREW_NO_ANALYTICS=1
+
+echo "Installing Homebrew packages..."
+
+packages=(
+    nvm               # Node version manager
+    pyenv             # Python version manager
+    heroku            # Heroku CLI
+    postgres          # Postgres DB
+    mongodb-community # MongoDB
+)
+
+for i in "${packages[@]}"; do
+    brew install $i
+done
