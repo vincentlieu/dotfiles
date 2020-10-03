848bbae2eb89b60eacea9859aeb901abfa5c5e48
Vincent Lieu <v.lieu93@gmail.com>
2020-10-03 23:48:27 +1000

    renamed omzsh script to oh-my-zsh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,13 +1,15 @@
-# install oh-my-zsh
+echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-echo "Removing custom folder"
+
+# Remove oh-my-zsh custom folder from installation.
 rm -rf ~/.oh-my-zsh/custom
 
+# Create symlink to .oh-my-zsh/custom
 echo "Creating Symlink to oh-my-zsh/custom"
-ln -fs $(pwd)/.oh-my-zsh/custom ~/.oh-my-zsh/
+ln -fs ./oh-my-zsh/custom ~/.oh-my-zsh/
 
 echo "Removing .zshrc from oh-my-zsh installation"
 # rm ~/.zshrc
 
 echo "Update submodules"
-git submodule init && git submodule update
+git submodule update --init --recursive
