b4e6fe0d622359b6ffc78d8e353630dcf8d1515b
Vincent Lieu <v.lieu93@gmail.com>
2020-10-03 21:24:58 +1000

    added --unattended arg to oh-my-zsh install

--- omzsh.sh
+++ omzsh.sh
@@ -1,6 +1,13 @@
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+echo "Removing custom folder"
 rm -rf ~/.oh-my-zsh/custom
+
+echo "Creating Symlink to oh-my-zsh/custom"
 ln -fs $(pwd)/.oh-my-zsh/custom ~/.oh-my-zsh/
+
+echo "Removing .zshrc from oh-my-zsh installation"
 rm ~/.zshrc
+
+echo "Update submodules"
 git submodule init && git submodule update
