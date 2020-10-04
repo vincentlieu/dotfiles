b50e2d0fdc8d46008cf7382a444d7a016dcebdc6
Vincent Lieu <v.lieu93@gmail.com>
2020-10-04 11:05:35 +1100

    Added symlink for .zshrc

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -5,8 +5,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 rm -rf ~/.oh-my-zsh/custom
 
 # Create symlink to .oh-my-zsh/custom
-echo "Creating Symlink to oh-my-zsh/custom"
+echo "Creating symlink to oh-my-zsh/custom"
 ln -fs $(pwd)/oh-my-zsh/custom ~/.oh-my-zsh/
 
-echo "Removing .zshrc from oh-my-zsh installation"
-# rm ~/.zshrc
+# Remove oh-my-zsh .zshrc from installation.
+rm ~/.zshrc
+
+echo "Creating symlink to ~/.zshrc"
+ln -s $(pwd)/.zshrc ~/.zshrc
