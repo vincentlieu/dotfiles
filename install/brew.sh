cdca6a8d4e195e1346d52827f6018539f046a2f6
Vincent Lieu <v.lieu93@gmail.com>
2020-12-04 23:44:00 +1100

    Fixed homebrew install command

--- install/brew.sh
+++ install/brew.sh
@@ -3,7 +3,7 @@
 # Install Homebrew
 if ! [ -x "$(which brew)" ]; then
     echo "[ INFO ] Homebrew not found. Installing Homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     echo "[ INFO ] Homebrew already installed. Updating..."
     brew update
