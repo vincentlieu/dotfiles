0d0347c429d9f8118ba0da7baa77eaba4699a740
Vincent Lieu <58590087+vincentlieu@users.noreply.github.com>
2024-05-03 23:08:46 +1000

    remove symlink functions from asdf script

--- asdf/asdf.sh
+++ asdf/asdf.sh
@@ -1,54 +1,5 @@
 #! /bin/bash
 
-default_packages_dir="/default-packages"
-
-# Add asdf.sh to $HOME/.asdfrc.
-# https://asdf-vm.com/guide/getting-started.html#_3-install-asdf
-function set_asdf_source() {
-    if ! command -v asdf &> /dev/null; then
-        echo "asdf doesn't seem to be installed. Cannot set source in zshrc."
-        exit
-    fi
-
-    asdf_source="$(brew --prefix asdf)/libexec/asdf.sh"
-    if grep -q "$asdf_source" "${ZDOTDIR:-~}/.zshrc"; then
-        echo "asdf source already exists."
-    else 
-        echo -e "\n. $asdf_source" >> "${ZDOTDIR:-~}"/.zshrc
-    fi
-    
-    return
-}
-
-# Symlinks .asdfrc to $HOME/.asdfrc.
-function set_asdfrc() {
-    if ! command -v asdf &> /dev/null; then
-        echo "Will not symlink .asdfrc since asdf does not exist."
-        exit
-    fi
-
-    asdfrc_file_name=".asdfrc"
-    asdfrc_file_path="$(pwd)/$asdfrc_file_name"    
-    # Check that the .asdfrc file exists.
-    if ! test -f "$asdfrc_file_path"; then
-        echo "Could not find .asdfrc file to symlink."
-        echo "PATH: $asdfrc_file_path"
-    else
-        if test -h "$HOME/$asdfrc_file_path"; then
-            echo ".asdfrc already symlinked."
-            return
-        fi
-
-        echo "Removing any references to .asdfrc"
-        rm "$HOME/$asdfrc_file_name"
-        
-        echo "Symlinking .asdfrc"
-        ln -s "$asdfrc_file_path" "$HOME"
-    fi
-
-    return
-}
-
 # Gets the asdf-vm node plugin source.
 # https://github.com/asdf-vm/asdf-plugins
 function get_nodejs_repository() {
@@ -60,30 +11,6 @@ function get_nodejs_repository() {
     # Short-form alternative: `asdf plugin add nodejs`
     # Recommended to use the long-form. 
     asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-    return
-}
-
-# Symlinks the default-npm-packages file in ../default-packages.
-function symlink_default_nodejs_packages() {
-    asdf_default_npm_file_name=".default-npm-packages"
-    asdf_default_npm_file_path="$(pwd)$default_packages_dir/$asdf_default_npm_file_name"
-
-    # Check that the default-npm-packages file exists.
-    if ! test -f "$asdf_default_npm_file_path"; then
-        echo "Could not find default-npm-packages file to symlink."
-        echo "PATH: $asdf_default_npm_file_path"
-    else
-        if test -h "$HOME/$asdf_default_npm_file_name"; then
-            echo "asdf default-npm-packages already symlinked."
-            return
-        fi
-
-        echo "Removing any references to .default-npm-packages"
-        rm "$HOME/$asdf_default_npm_file_name"
-        
-        echo "Symlinking asdf default-npm-packages"
-        ln -s "$asdf_default_npm_file_path" "$HOME"
-    fi
     return
 }
 
@@ -101,46 +28,8 @@ function get_python_repository() {
     return
 }
 
-# Symlinks the default-python-packages file in ../default-packages.
-function symlink_default_python_packages() {
-    asdf_default_python_file_name=".default-python-packages"
-    asdf_default_python_file_path="$(pwd)$default_packages_dir/$asdf_default_python_file_name"
-
-    # Check that the default-python-packages file exists.
-    if ! test -f "$asdf_default_python_file_path"; then
-        echo "Could not find default-python-packages file to symlink."
-        echo "PATH: $asdf_default_python_file_path"
-    else
-        if test -h "$HOME/$asdf_default_python_file_name"; then
-            echo "asdf default-python-packages already symlinked."
-            return
-        fi
-        
-        echo "Removing any references to .default-python-packages"
-        rm "$HOME/$asdf_default_python_file_name"
-
-        echo "Symlinking asdf default-python-packages"
-        ln -s "$asdf_default_python_file_path" "$HOME"
-    fi
-    return
-}
-
-# Sets up everything required for the python plugin.
-function setup_python_plugin() {
-    symlink_default_python_packages
-    get_python_repository
-    return
-}
-
-# Sets up everything required for the nodejs plugin.
-function setup_nodejs_plugin() {
-    symlink_default_nodejs_packages
-    get_nodejs_repository
-    return
-}
-
-# Sets up all plugins.
-function setup_asdf_plugins() {
+# Get plugins.
+function get_asdf_plugins() {
     if ! command -v asdf &> /dev/null; then
         echo "Cannot run 'asdf plugin add'"
         exit
@@ -150,9 +39,9 @@ function setup_asdf_plugins() {
     for plugin in "${asdf_plugins[@]}"; do
         case $plugin in
         "nodejs" | "node")
-            setup_nodejs_plugin;;
+            get_nodejs_repository;;
         "python")
-            setup_python_plugin;;
+            get_python_repository;;
         *)
            echo "Unknown asdf plugin specified. PLUGIN: $plugin"
            ;;
@@ -161,23 +50,3 @@ function setup_asdf_plugins() {
 
     return
 }
-
-# Sets up asdf configuration.
-function setup_asdf_config() {
-    set_asdf_source
-    set_asdfrc
-    return
-}
-
-function main() {
-    if ! command -v asdf &> /dev/null; then
-        echo "asdf is not installed."
-        exit
-    fi
-
-    setup_asdf_config
-    setup_asdf_plugins
-    return
-}
-
-main
